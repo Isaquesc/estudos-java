895aaff3ac6f4136ea90b223b9c65cdf8bf12664
ISAQUE DA SILVA COSTA <218532019@eniac.edu.br>
2023-09-14 02:21:40 -0300

    141 - Classes Utilitárias - IO pt 04 - BufferedWriter

---
+++ estudosJAVA/src/iniciante/javaIOdevDojo/BufferedWriterTest01.java
@@ -0,0 +1,24 @@
+package iniciante.javaIOdevDojo;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class BufferedWriterTest01 {
+    public static void main(String[] args) {
+        File file = new File("C:\\Users\\iscosta2\\Documents\\ProjetosPessoais\\estudosJAVA\\estudosJAVA\\arquivo\\file.txt");
+        try(FileWriter fileWriter = new FileWriter(file)){
+
+            BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
+            bufferedWriter.write("Estou no modo foco total (academia, estudos, noiva, casa)");
+            bufferedWriter.newLine();
+            bufferedWriter.write("pulando linha com bufferedWriter");
+            bufferedWriter.flush();
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+}
