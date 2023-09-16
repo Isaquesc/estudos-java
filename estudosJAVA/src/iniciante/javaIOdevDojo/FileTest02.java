22f53b3c8cfe326f05f434dee2a5faaa7689ed21
ISAQUE DA SILVA COSTA <218532019@eniac.edu.br>
2023-09-16 07:30:27 -0300

    143 - Classes Utilitárias - IO pt 06 - File para diretórios

---
+++ estudosJAVA/src/iniciante/javaIOdevDojo/FileTest02.java
@@ -0,0 +1,28 @@
+package iniciante.javaIOdevDojo;
+
+import java.io.File;
+import java.io.IOException;
+import java.time.Instant;
+import java.time.ZoneId;
+
+public class FileTest02 {
+    public static void main(String[] args) throws IOException {
+
+        //criando um diretorio
+        File pasta = new File("C:\\Users\\iscosta2\\Documents\\ProjetosPessoais\\estudosJAVA\\estudosJAVA\\arquivo\\pasta");
+        System.out.println("pasta criada? - " + pasta.mkdir());
+
+        //criando um arquivo com um objeto de diretorio
+        File file = new File(pasta, "arquivo.txt");
+        file.createNewFile();
+        System.out.println("arquivo.txt criado? - " + file.exists());
+
+        //renomeando um arquivo
+        boolean b = file.renameTo(new File(pasta, "arquivoRenomeado.txt"));
+        System.out.println("arquivo.txt renomeado? - " + b);
+
+        //renomeando uma pasta
+        boolean b1 = pasta.renameTo(new File("C:\\Users\\iscosta2\\Documents\\ProjetosPessoais\\estudosJAVA\\estudosJAVA\\arquivo\\pastaRenomeada"));
+        System.out.println("pasta renomeada? - " + b1);
+    }
+}
