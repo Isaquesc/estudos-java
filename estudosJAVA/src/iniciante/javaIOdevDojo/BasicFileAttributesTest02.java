f7a200b7318fd9b16ce6237fea93aefd28fb2701
ISAQUE DA SILVA COSTA <218532019@eniac.edu.br>
2023-10-26 14:50:22 -0300

    151 - Classes Utilitárias - NIO pt 08 - DosFileAttribute

--- estudosJAVA/src/iniciante/javaIOdevDojo/BasicFileAttributesTest02.java
+++ estudosJAVA/src/iniciante/javaIOdevDojo/BasicFileAttributesTest02.java
@@ -6,28 +6,45 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.attribute.BasicFileAttributeView;
 import java.nio.file.attribute.BasicFileAttributes;
+import java.nio.file.attribute.FileTime;
+import java.time.Clock;
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.util.Calendar;
+import java.util.stream.Stream;
 
 public class BasicFileAttributesTest02 {
 
     public static void main(String[] args) throws IOException {
 
-        Path path = Paths.get("C:\\Users\\iscosta2\\Documents\\ProjetosPessoais\\estudosJAVA\\estudosJAVA\\arquivo", "file.txt");
-        Files.createFile(path);
+        Path path = Paths.get("arquivoRenomeado.txt");
 
-        //fazendo a leitura do arquivo (apenas LER)
-        BasicFileAttributes readAttributes = Files.readAttributes(path, BasicFileAttributes.class);
+        //criando um objeto do tipo BasicFileAttributes para conseguir ler o patch mencionado
+        LeituraPropriedades.getPropriedades(path).forEach(System.out::println);;
 
-        System.out.println(readAttributes.creationTime());
-        System.out.println(readAttributes.lastAccessTime());
-        System.out.println(readAttributes.lastModifiedTime());
-
-        //pegando o arquivo para conseguir alterar informações
+        //criando um objetivo do tipo BasicFileAttributesView, para consegue editar o patch mencionado.
         BasicFileAttributeView fileAttributeView = Files.getFileAttributeView(path, BasicFileAttributeView.class);
 
 
+        System.out.println(ZoneId.systemDefault());
 
+        fileAttributeView.setTimes(null, null, FileTime.from(Instant.parse("2023-10-26T11:00:00Z")));
+        System.out.println("-----------------------");
+        LeituraPropriedades.getPropriedades(path).forEach(System.out::println);
+    }
 
+    class LeituraPropriedades {
 
-        Files.deleteIfExists(path);
+        static Stream<FileTime> getPropriedades(Path path) throws IOException {
+            BasicFileAttributes leitura = Files.readAttributes(path, BasicFileAttributes.class);
+
+            Stream<FileTime> fileTimeStream = Stream.of(leitura.creationTime(),
+                    leitura.lastAccessTime(),
+                    leitura.lastModifiedTime());
+
+            return fileTimeStream;
+        }
+
     }
 }
