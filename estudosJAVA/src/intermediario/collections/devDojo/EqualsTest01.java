a5a45fef44e8bd04ea1eeeba65daa7aa6c8e1f3e
ISAQUE DA SILVA COSTA <218532019@eniac.edu.br>
2024-01-11 19:54:43 -0300

    161 - Coleções pt 01 - equals pt 01

---
+++ estudosJAVA/src/intermediario/collections/devDojo/EqualsTest01.java
@@ -0,0 +1,12 @@
+package intermediario.collections.devDojo;
+
+import intermediario.collections.devDojo.dominio.SmartPhone;
+
+public class EqualsTest01 {
+    public static void main(String[] args) {
+
+        SmartPhone s1 = new SmartPhone("1AQ63W79", "Apple");
+        SmartPhone s2 = new SmartPhone("1AQ63W79", "Apple");
+        System.out.println(s1.getSerialNumber().equals(s2.getSerialNumber()));
+    }
+}
