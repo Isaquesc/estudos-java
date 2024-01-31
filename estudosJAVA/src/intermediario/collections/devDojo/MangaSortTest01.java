c240d94670901e279d00e58b7bcb3f89da6dba03
ISAQUE DA SILVA COSTA <218532019@eniac.edu.br>
2024-01-31 16:16:29 -0300

    171 - Coleções pt 11 - Sorting lists pt 03 - Comparator

--- estudosJAVA/src/intermediario/collections/devDojo/MangaSortTest01.java
+++ estudosJAVA/src/intermediario/collections/devDojo/MangaSortTest01.java
@@ -4,7 +4,15 @@ import intermediario.collections.devDojo.dominio.Serie;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
+
+class SerieSorteByID implements Comparator<Serie> {
+    @Override
+    public int compare(Serie o1, Serie o2) {
+        return o1.getId().compareTo(o2.getId());
+    }
+}
 
 public class MangaSortTest01 {
     public static void main(String[] args) {
@@ -17,7 +25,8 @@ public class MangaSortTest01 {
         series.add(new Serie(2L, "Grey's Anatomy", 4));
         series.add(new Serie(5L, "Grey's Anatomy", 5));
 
-        Collections.sort(series);
+//        Collections.sort(series, new SerieSorteByID());
+        series.sort(new SerieSorteByID());
         System.out.println(series);
 
     }
