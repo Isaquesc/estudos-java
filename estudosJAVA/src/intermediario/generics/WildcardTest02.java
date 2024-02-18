58e83bd8065085603023fe43cf6b6c05f183cbce
ISAQUE DA SILVA COSTA <218532019@eniac.edu.br>
2024-02-18 14:09:12 -0300

    186 - Generics pt 02 - WildCard pt 2

--- estudosJAVA/src/intermediario/generics/WildcardTest02.java
+++ estudosJAVA/src/intermediario/generics/WildcardTest02.java
@@ -1,18 +1,29 @@
 package intermediario.generics;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class WildcardTest02 {
     public static void main(String[] args) {
         List<Cachorro> cachorros = List.of(new Cachorro(), new Cachorro());
         List<Gato> gatos = List.of(new Gato(), new Gato());
+        List<Animal> animalList = new ArrayList<>();
 
-//        printConsulta(cachorros);
+        printConsulta(cachorros);
+        printConsultaAdicionar(animalList);
     }
 
-    private static void printConsulta(List<Animal> animals) {
+    private static void printConsulta(List<? extends Animal> animals) {
         for (Animal animal : animals)
             animal.consulta();
     }
 
+    private static void printConsultaAdicionar(List<? super Animal> animals) {
+        animals.add(new Gato());
+        animals.add(new Cachorro());
+
+        for (Object o : animals)
+            if (o instanceof Animal)
+                ((Animal) o).consulta();
+    }
 }
