be3e4db364e7d88d385c3acaad847b26f4e6e343
ISAQUE DA SILVA COSTA <218532019@eniac.edu.br>
2024-02-18 17:33:07 -0300

    191 - Classes Internas pt 03 - Classes Anônimas

--- estudosJAVA/src/intermediario/classeAnonima/ClasseAnonymous.java
+++ estudosJAVA/src/intermediario/classeAnonima/ClasseAnonymous.java
@@ -1,2 +1,35 @@
-package intermediario.classeAnonima;public class ClasseAnonymous {
+package intermediario.classeAnonima;
+
+import intermediario.generics.dominio.Barco;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
+public class ClasseAnonymous {
+
+    class ComparatorBarcoByNome implements Comparator<Barco>{
+
+        @Override
+        public int compare(Barco o1, Barco o2) {
+            return o1.getNome().compareTo(o2.getNome());
+        }
+    }
+    public static void main(String[] args) {
+
+        List<Barco> barcos = new ArrayList<>(List.of(new Barco("Lancha"), new Barco("Canoa")));
+
+        //ANTIGA SINTAX
+        barcos.sort(new Comparator<Barco>() {
+            @Override
+            public int compare(Barco o1, Barco o2) {
+                return o1.getNome().compareTo(o2.getNome());
+            }
+        });
+
+        //NOVA SINTAX
+        barcos.sort(((o1, o2) -> o1.getNome().compareTo(o2.getNome())));
+
+        System.out.println(barcos);
+    }
 }
--- estudosJAVA/src/intermediario/generics/MetodoGenericoTest01.java
+++ estudosJAVA/src/intermediario/generics/MetodoGenericoTest01.java
@@ -2,17 +2,20 @@ package intermediario.generics;
 
 import intermediario.generics.dominio.Barco;
 
+import java.util.Comparator;
 import java.util.List;
 
 public class MetodoGenericoTest01 {
     public static void main(String[] args) {
         List<Barco> barcoList = criarUmaListComUmObjeto(new Barco("Canoa Azul"));
         System.out.println(barcoList);
-
-
     }
 
     private static <T> List<T> criarUmaListComUmObjeto(T t){
+        return List.of(t);
+    }
+
+    private static <T extends Comparator> List<T> criarUmaListComUmObjeto(T t){
         return List.of(t);
     }
 }
