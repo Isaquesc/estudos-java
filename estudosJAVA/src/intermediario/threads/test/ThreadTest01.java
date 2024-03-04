7cb38c8a9b79e71a657cba248968b1372b580e3e
ISAQUE DA SILVA COSTA <218532019@eniac.edu.br>
2024-03-04 17:05:07 -0300

    222 - Threads pt 03 - Prioridade e Sleep

--- estudosJAVA/src/intermediario/threads/test/ThreadTest01.java
+++ estudosJAVA/src/intermediario/threads/test/ThreadTest01.java
@@ -28,12 +28,16 @@ class ThreadExampleRunnable implements Runnable {
 
     @Override
     public void run() {
-        System.out.println("Inicio da thread: " + Thread.currentThread().getName());
+        System.out.print(Thread.currentThread().getName());
         for (int i = 0; i < 10; i++) {
             System.out.print(c);
-            System.out.println();
+
+            try {
+                Thread.sleep(2000);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
         }
-        System.out.println("FIM da thread: " + Thread.currentThread().getName());
     }
 }
 public class ThreadTest01 {
@@ -44,15 +48,18 @@ public class ThreadTest01 {
 //        ThreadExemple t3 = new ThreadExemple('C');
 //        ThreadExemple t4 = new ThreadExemple('D');
 
-        Thread t1 = new Thread(new ThreadExampleRunnable('A'));
-        Thread t2 = new Thread(new ThreadExampleRunnable('B'));
-        Thread t3 = new Thread(new ThreadExampleRunnable('C'));
-        Thread t4 = new Thread(new ThreadExampleRunnable('D'));
+        Thread t1 = new Thread(new ThreadExampleRunnable('A'), " T1 ");
+        Thread t2 = new Thread(new ThreadExampleRunnable('B'), " T2 ");
+        Thread t3 = new Thread(new ThreadExampleRunnable('C'), " T3 ");
+        Thread t4 = new Thread(new ThreadExampleRunnable('D'), " T4 ");
 
+        t4.setPriority(Thread.MAX_PRIORITY);
         t1.start();
         t2.start();
         t3.start();
         t4.start();
+
+        System.out.println("############################ Main");
 
     }
 }
