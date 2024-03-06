463541f64d4e00904746b95544e5c140741c832a
ISAQUE DA SILVA COSTA <218532019@eniac.edu.br>
2024-03-06 17:56:41 -0300

    225 - Threads pt 06 - Sincronismo de thread pt 02

--- estudosJAVA/src/intermediario/threads/test/ThreadAccountTest01.java
+++ estudosJAVA/src/intermediario/threads/test/ThreadAccountTest01.java
@@ -6,16 +6,12 @@ public class ThreadAccountTest01 implements Runnable{
     private Account account = new Account();
 
     public static void main(String[] args) throws InterruptedException {
+
         ThreadAccountTest01 threadAccountTest01 = new ThreadAccountTest01();
-
-
         Thread t1 = new Thread(threadAccountTest01, "Isaque");
         Thread t2 = new Thread(threadAccountTest01, "Jessica");
-
         t1.start();
-        t1.join();
         t2.start();
-
     }
 
     @Override
@@ -25,16 +21,20 @@ public class ThreadAccountTest01 implements Runnable{
             if (account.getBalance() < 0)
                 System.out.println("ZEROU A CONTA");
         }
-
     }
 
     private void withdrawl(int amount) {
-        if (account.getBalance() >= amount) {
-            System.out.println(getNameThread() + " está indo sacar dinheiro");
-            account.withDrawl(amount);
-            System.out.println(getNameThread() + " - saque realizado com sucesso - SALDO ATUAL: " + account.getBalance());
-        } else {
-            System.out.println("Desculpe " + getNameThread() + " Saldo insuficiente: " + account.getBalance());
+        System.out.println(getNameThread() +" #### fora do synchronized");
+
+        synchronized (account) {
+            System.out.println(getNameThread() +" **** dentro do synchronized");
+            if (account.getBalance() >= amount) {
+                System.out.println(getNameThread() + " está indo sacar dinheiro");
+                account.withDrawl(amount);
+                System.out.println(getNameThread() + " - saque realizado com sucesso - SALDO ATUAL: " + account.getBalance());
+            } else {
+                System.out.println("Desculpe " + getNameThread() + " Saldo insuficiente: " + account.getBalance());
+            }
         }
     }
 
