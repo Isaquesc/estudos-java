888097473bd25424386676afe69d306205639422
ISAQUE DA SILVA COSTA <218532019@eniac.edu.br>
2024-03-06 15:46:48 -0300

    225 - Threads pt 06 - Sincronismo de thread pt 01

---
+++ estudosJAVA/src/intermediario/threads/test/ThreadAccountTest01.java
@@ -0,0 +1,44 @@
+package intermediario.threads.test;
+
+import intermediario.threads.dominio.Account;
+
+public class ThreadAccountTest01 implements Runnable{
+    private Account account = new Account();
+
+    public static void main(String[] args) throws InterruptedException {
+        ThreadAccountTest01 threadAccountTest01 = new ThreadAccountTest01();
+
+
+        Thread t1 = new Thread(threadAccountTest01, "Isaque");
+        Thread t2 = new Thread(threadAccountTest01, "Jessica");
+
+        t1.start();
+        t1.join();
+        t2.start();
+
+    }
+
+    @Override
+    public void run() {
+        for (int i = 0; i < 5; i++) {
+            withdrawl(10);
+            if (account.getBalance() < 0)
+                System.out.println("ZEROU A CONTA");
+        }
+
+    }
+
+    private void withdrawl(int amount) {
+        if (account.getBalance() >= amount) {
+            System.out.println(getNameThread() + " está indo sacar dinheiro");
+            account.withDrawl(amount);
+            System.out.println(getNameThread() + " - saque realizado com sucesso - SALDO ATUAL: " + account.getBalance());
+        } else {
+            System.out.println("Desculpe " + getNameThread() + " Saldo insuficiente: " + account.getBalance());
+        }
+    }
+
+    private String getNameThread() {
+        return Thread.currentThread().getName();
+    }
+}
